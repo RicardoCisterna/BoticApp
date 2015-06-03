596549bc294b11c7ed844e1f4707b51e66f427fe
eliasplease <elias.sobarzo@usach.cl>
2015-06-03 05:06:35 -0300

    Se agregan pantalla inicial

--- app/src/main/java/com/boticap/boticapp/BusquedaFarmacia.java
+++ app/src/main/java/com/boticap/boticapp/BusquedaFarmacia.java
@@ -6,7 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class BusquedaFarmacia extends ActionBarActivity {
+public class BusquedaFarmacia extends ActionBarActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
